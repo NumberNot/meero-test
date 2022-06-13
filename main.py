94d744080cc8fc3d4fab731179b9e082b0544a1e
Artem Kokorin <stub@email.here>
2022-06-13 19:47:17 +0300

    intercept endless intervals

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
+from ast import Index
+import datetime as dt
 
-today = datetime.date.today()
+today = dt.date.today()
 
 file_name = "log.txt"
 file = open(file_name, "r")
@@ -20,13 +21,23 @@ for line in file.readlines():
     structure = {key:value for key, value in zip(process, details)}
     data.append(structure)
 
-# time = [datetime.datetime.strptime(
+# time = [dt.datetime.strptime(
 #     sub['date'], '%Y-%m-%dT%H:%M') for sub in data]
 res = set([sub['name'] for sub in data])
 
 for item in res:
     occur(item)
     print(unique_lst)
-    
+    for items in range(len(unique_lst)):
+        a = [sub['condition'] for sub in unique_lst]
+        try:
+            if str(a[items]) == "Start" and str(a[items+1]) == "End":
+                print(a[items])
+                print(a[items+1])
+        except:
+            print("No end")
+        time_period = [dt.datetime.strptime(
+            sub['date'], '%Y-%m-%dT%H:%M') for sub in unique_lst]
+        # print(time_period[items])
     unique_lst = []
 
