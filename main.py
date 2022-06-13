129339ec58e14c16fc0fc80b2c233a6ddaf38e51
Artem Kokorin <stub@email.here>
2022-06-13 18:56:39 +0300

    concoct unique lists

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import json
+import datetime
+
+today = datetime.date.today()
 
 file_name = "log.txt"
 file = open(file_name, "r")
 data = []
-process = ["date", "time", "operation", "name", "condition"]
+unique_lst = []
+process = ["date", "operation", "name", "condition"]
+
+def occur (unique_item):
+    for unique_item in data:
+        if unique_item["name"] == item:
+            unique_lst.append(unique_item)
+    return(unique_lst)
 
 for line in file.readlines():
-    line = line.replace("T", " ",1)
     details = line.split(" ")
     details = [x.strip() for x in details]
     structure = {key:value for key, value in zip(process, details)}
     data.append(structure)
-    # data.append(details)
-print(data)
-# for entry in data:
-#     print(json.dumps(entry, indent = 4))
+
+# time = [datetime.datetime.strptime(
+#     sub['date'], '%Y-%m-%dT%H:%M') for sub in data]
+res = set([sub['name'] for sub in data])
+
+for item in res:
+    occur(item)
+    print(unique_lst)
+    
+    unique_lst = []
 
